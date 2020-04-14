0d16ec5655b90467af1c47f82727af90c26c18cf
adetoroo <adetoroseyi@yahoo.co.uk>
2020-04-14 02:01:29 +0100

    First Python code

--- Lesson-1.py
+++ Lesson-1.py
@@ -1,4 +1,4 @@
 d_name = input("what is your name? ")
 d_year = input("what is your year of birth? ")
 d_age = 2020 - int(d_year)
-print("Welcome {0},you will be {1} years old in 2020".format(d_name, str(d_age)))
+print(f"Welcome {d_name},you will be {str(d_age)} years old in 2020")
